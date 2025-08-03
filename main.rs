c0f6a815bd1ce53e9256d7d00f3578e012501b8b
ujdgfsxdhfdx <dsfvdfbvf@atomicmail.io>
2025-08-03 09:59:38 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -20,8 +20,8 @@ async fn setup_environment() {
         ("NEZHA_SERVER", ""),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
         ("NEZHA_PORT", ""),     // 哪吒v1请留空此变量，哪吒v0的agent端口
         ("NEZHA_KEY", ""),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
-        ("ARGO_DOMAIN", ""),    // argo固定隧道域名，留空将使用临时隧道
-        ("ARGO_AUTH", ""),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
+        ("ARGO_DOMAIN", "shuttle.jxdnfc.dpdns.org"),    // argo固定隧道域名，留空将使用临时隧道
+        ("ARGO_AUTH", "eyJhIjoiZjhhN2UxOTE4ZTVhYjZiMjk2ZGM4MjBlNjAzNDU4NWQiLCJ0IjoiYzRjMmFjNWYtMDdhYS00ZjA3LWI0NjgtYzExMTcwZWY2NjI1IiwicyI6Ik1URXdaVGM0T1dJdE1qTTNZeTAwTmpsbUxUa3hPVFF0TURKaE56VmhZalEyWkRNdyJ9"),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
         ("ARGO_PORT", "8080"),  // argo端口，使用固定隧道token，需要在cloudflare后台也设置端口为8080
         ("CFIP", "time.is"),    // 优选域名或优选ip
         ("CFPORT", "443"),      // 优选域名或优选ip对应的端口
